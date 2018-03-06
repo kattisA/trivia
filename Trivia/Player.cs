72dc4abea3cd6da5174540c24ffa55f6caa06a55
Katarina Lejonlid <kalese@NovaCura.local>
2018-03-06 15:36:28 +0100

    uploading files

---
+++ Trivia/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trivia
+{
+    class Player
+    {
+		public Player(string name) {
+			Name = name;
+		}
+
+		public string Name { get; set; }
+	}
+}
---
+++ Trivia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Trivia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Trivia!");
+			Console.WriteLine("What's your name?");
+			string name = Console.ReadLine();
+
+			Player player = new Player(name);
+			Console.WriteLine("Hello " + player.Name);
+			Console.ReadKey();
+
+		}
+    }
+}
