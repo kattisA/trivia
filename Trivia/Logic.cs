e2cafdfdd1bcb49208a99134918e3576402822cb
Katarina Lejonlid <kalese@NovaCura.local>
2018-03-16 13:04:38 +0100

    Logic, a menu and a start of addition

---
+++ Trivia/Logic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trivia
+{
+    class Logic
+    {
+		public Logic() { }
+
+		public int GenerateRandom() {
+
+			Random random = new Random();
+			int rand_number = random.Next(1, 11);
+			return rand_number;
+			
+		}
+
+		public void playAdditionTrivia() {
+			int number1 = GenerateRandom();
+			int number2 = GenerateRandom();
+
+			Console.WriteLine(AdditionQuestion(number1, number2));
+			string answerInput = Console.ReadLine();
+			int answer = Convert.ToInt32(answerInput);
+			int correct = number1 + number2;
+
+			if (answer == correct)
+			{
+				Console.WriteLine("Well done");
+			}
+			else { Console.WriteLine("Sorry, better luck next time!"); }
+		}
+
+		public string AdditionQuestion(int num1, int num2) {
+			string question = "What is " + num1 + " + " + num2 + " ?";
+			return question;
+		}
+    }
+}
--- Trivia/Program.cs
+++ Trivia/Program.cs
@@ -6,14 +6,50 @@ namespace Trivia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Trivia!");
+			Logic logic = new Logic();
+			int userInput = 0;
+			Console.WriteLine("Welcome to Trivia!");
 			Console.WriteLine("What's your name?");
 			string name = Console.ReadLine();
 
 			Player player = new Player(name);
 			Console.WriteLine("Hello " + player.Name);
-			Console.ReadKey();
+
+			do
+			{
+				userInput = Menu();
+				switch (userInput)
+				{
+					case 1:
+						Console.WriteLine("Case 1: You have chosen addition!");
+						logic.playAdditionTrivia();
+						break;
+					case 2:
+						Console.WriteLine("Case 2");
+						break;
+					case 3:
+						Console.WriteLine("Case 3");
+						break;
+					case 4:
+						Console.WriteLine("Case 4");
+						break;
+					default:
+						Console.WriteLine("Please enter a correct choice");
+						break;
+				}
+			} while (userInput != 5);
 
 		}
-    }
+
+		static public int Menu()
+		{
+			Console.WriteLine("1. Addition");
+			Console.WriteLine("2. ");
+			Console.WriteLine("3. ");
+			Console.WriteLine("4. ");
+			Console.WriteLine("5. Exit");
+			var result = Console.ReadLine();
+			return Convert.ToInt32(result);
+		}
+	}
 }
